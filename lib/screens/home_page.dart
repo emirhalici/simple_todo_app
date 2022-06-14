aacf6f9393b634b9effceb911ecb5f5fba316e53
emirhalici <emir.halici1@gmail.com>
2022-06-14 12:52:24 +0300

    add empty state screen when there todo list is empty

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 import 'package:simple_todo_app/models/todo_model.dart';
 import 'package:simple_todo_app/project_utils.dart';
 import 'package:simple_todo_app/providers/main_provider.dart';
 import 'package:simple_todo_app/widgets/add_todo_sheet.dart';
 import 'package:simple_todo_app/widgets/edit_todo_sheet.dart';
+import 'package:simple_todo_app/widgets/empty_state_widget.dart';
 import 'package:simple_todo_app/widgets/todo_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -62,34 +64,36 @@ class _HomePageState extends State<HomePage> {
       ),
       body: context.watch<MainProvider>().mainTodos == null
           ? ProjectUtils.circularProgressBar(context)
-          : ListView.builder(
-              itemCount: context.watch<MainProvider>().mainTodos?.length,
-              itemBuilder: (context, index) => TodoCard(
-                todoModel: context.watch<MainProvider>().mainTodos![index],
-                onTappedCallback: (TodoModel todoModel) => showModalBottomSheet(
-                  context: context,
-                  builder: (context) => EditTodoSheet(
-                    todoModel: TodoModel.copyFrom(context.read<MainProvider>().mainTodos![index]),
+          : context.watch<MainProvider>().mainTodos!.isEmpty
+              ? const EmptyStateWidget()
+              : ListView.builder(
+                  itemCount: context.watch<MainProvider>().mainTodos?.length,
+                  itemBuilder: (context, index) => TodoCard(
+                    todoModel: context.watch<MainProvider>().mainTodos![index],
+                    onTappedCallback: (TodoModel todoModel) => showModalBottomSheet(
+                      context: context,
+                      builder: (context) => EditTodoSheet(
+                        todoModel: TodoModel.copyFrom(context.read<MainProvider>().mainTodos![index]),
+                      ),
+                    ),
+                    onCheckedCallback: (bool val) async {
+                      TodoModel originalModel = context.read<MainProvider>().mainTodos![index];
+                      TodoModel copy = TodoModel.copyFrom(originalModel);
+                      copy.isDone = val;
+                      context.read<MainProvider>().mainTodos![index] = copy;
+                      try {
+                        String response = await context.read<MainProvider>().editTodo(copy);
+                        if (response != 'Success' && mounted) {
+                          context.read<MainProvider>().mainTodos![index] = originalModel;
+                          showSnackBarMessage('Error editing todo');
+                        }
+                      } catch (e) {
+                        context.read<MainProvider>().mainTodos![index] = originalModel;
+                        showSnackBarMessage('Error editing todo');
+                      }
+                    },
                   ),
                 ),
-                onCheckedCallback: (bool val) async {
-                  TodoModel originalModel = context.read<MainProvider>().mainTodos![index];
-                  TodoModel copy = TodoModel.copyFrom(originalModel);
-                  copy.isDone = val;
-                  context.read<MainProvider>().mainTodos![index] = copy;
-                  try {
-                    String response = await context.read<MainProvider>().editTodo(copy);
-                    if (response != 'Success' && mounted) {
-                      context.read<MainProvider>().mainTodos![index] = originalModel;
-                      showSnackBarMessage('Error editing todo');
-                    }
-                  } catch (e) {
-                    context.read<MainProvider>().mainTodos![index] = originalModel;
-                    showSnackBarMessage('Error editing todo');
-                  }
-                },
-              ),
-            ),
     );
   }
 }
---
+++ lib/widgets/empty_state_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class EmptyStateWidget extends StatelessWidget {
+  const EmptyStateWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 160.h),
+          SvgPicture.asset(
+            'assets/images/empty_state.svg',
+            height: 220.h,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 22),
+            child: Text(
+              'No todos in the list. Try adding one from down below!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 22.sp,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
