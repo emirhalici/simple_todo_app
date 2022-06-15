07c3e2a057aa5112fe971267f9a2405ed0d259d2
emirhalici <emir.halici1@gmail.com>
2022-06-15 18:21:03 +0300

    show toasts instead of snackbars

--- lib/project_utils.dart
+++ lib/project_utils.dart
@@ -24,4 +24,16 @@ class ProjectUtils {
           backgroundColor: Theme.of(context).colorScheme.primary,
         ),
       );
+
+  static Widget toastWidget(BuildContext context, String message) => Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25.0),
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        child: Text(
+          message,
+          style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+        ),
+      );
 }
